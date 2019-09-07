6d8b67cbb62dbe3536145cab5e748769908b1bc0
André Moura <andre.benedicto@etec.sp.gov.br>
2019-09-07 14:33:46 -0300

    Criando estrutura para o site

--- index.php
+++ index.php
@@ -3,21 +3,45 @@
 <head>
 	<meta charset="utf-8"/>
 	<title>Padrão</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Site criado no curso de PHP com MySQL" />
+	<meta name="keywords" content="curso php mysql site exemplo">
+	<meta name="autor" content="André Moura">
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+	<link rel="stylesheet" href="css/folha_all.css">
+	<link rel="stylesheet" href="css/home.css">
 </head>
 <body>
 	<header>
-		<h1> Curso de PHP com MySQL </h1>
+		<img src="images/html-css-js-php-mysql.png" alt="">
+	
+		<nav>
+			<h1>PHP com banco de dados MySQL</h1>
+			<p>
+				<a href="#">Básico</a>
+				<a href="#">Sistema</a>
+			</p>
+		</nav>
 	</header>
-	<nav>
-		<div id="basico">
-			<h2> Exercícios Básicos </h2>
-		</div>
-		<div id="sistema">
-			<h2> Controle Financeiro </h2>
-		</div>
-	</nav>
+	
 	<main>
-		<p> O site demonstra tudo ou quase tudo o que foi visto no curso de PHP com banco de dados MySQL </p>
+		<article>
+			<h1>O site desenvolvido com curso de PHP com banco de dados MySQL</h1>
+			<hr>
+			<p>
+				O curso aborda todos conceitos básicos da linguagem de PHP.
+			</p>
+
+			<p>
+				Aborda os conceitos iniciais da linguagem de marcação HTML e de formatação CSS.
+			</p>
+
+			<p>
+				Mé faiz elementum girarzis, nisi eros vermeio. Viva Forevis aptent taciti sociosqu ad litora torquent. Casamentiss faiz malandris se pirulitá. Atirei o pau no gatis, per gatis num morreus.
+			</p>
+		</article>
+		
 	</main>
 	<footer>
 		<p>Site desenvolvido por André Moura</p>
